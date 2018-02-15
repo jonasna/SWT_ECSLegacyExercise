e6fd815cdb5b5ef5423fd26c5c3a7d7c26e94a5c
jonasna <jonasna93@gmail.com>
2018-02-15 11:42:45 +0100

    Added initial projects (including test project)

---
+++ ECS.Legacy.Unittests/HeaterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Unittests
+{
+    [TestFixture]
+    class HeaterTests
+    {
+        [Test]
+        public void SelfTest_ReturnsTrue()
+        {
+            var uut = new Heater();
+            Assert.That(uut.RunSelfTest(), Is.EqualTo(true));
+        }
+    }
+}
